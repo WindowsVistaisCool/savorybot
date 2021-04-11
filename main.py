9a403104051352cbd36c1b81f9fb76ee8612d326
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-04-11 19:25:42 -0400

    added all commands

--- main.py
+++ main.py
@@ -14,20 +14,27 @@ client.remove_command('help')
 slash = SlashCommand(client)
 header = store('config.json', 'token', True)
 
-async def getitem(ctx, item, time, *, username=None):
-	if username is None:
+async def getitem(ctx, item, time, *, username=None, rocks=False):
+	def genuser():
 		username = ''.join(random.choice(string.ascii_letters) for i in range(random.randint(3, 16)))
+		return username
+	if username is None:
+		genuser()
 	locations = ['Graveyard', 'Castle', 'Wizard Tower', 'Barn', 'Dark Auction', 'Auction House', 'Lumber Merchant', 'Plumber Joe\'s House', 'Community Center', 'Jacob\'s House', 'Catacombs Entrance', 'Coal Mines', 'Bank', 'Builder\'s House', 'Maddox the Slayer', 'Tia the Fairy']
 	location = random.choice(locations)
-	e = discord.Embed(title="A wild item appeared!", color=discord.Color.green(), description=f"Hurry to pick it up at the `{location}` in `Hub {random.randint(1, 40)}` before it dissapears!", timestamp=datetime.utcnow())
-	e.add_field(name=item, value=f"Dropped by `{username}`")
+	e = discord.Embed(title="Dropped item(s) were found!", color=discord.Color.green(), description=f"Hurry to pick it up at the `{location}` in `Hub {random.randint(1, 40)}` before it dissapears!", timestamp=datetime.utcnow())
+	if rocks is True:
+		for x in range(8):
+			e.add_field(name=f"`{item}` ({x})", value=f"Dropped by `{genuser}`")
+	else:
+		e.add_field(name=f"`{item}`", value=f"Dropped by `{genuser}`")
 	e.set_footer(text=f"{time} seconds!")
 	d = await ctx.send(embed=e)
 	await sleep(time)
 	g = discord.Embed(title="The item disappeared!", color=discord.Color.red())
 	e.set_footer(text="Rip item")
 	await d.edit(embed=g)
-	await sleep(time)
+	await sleep(10)
 	await d.delete()
 
 @client.event
@@ -48,13 +55,29 @@ async def on_command_error(ctx, error):
 		await ctx.send(embed=e)
 
 #slashcommands
+@slash.slash(name='apply')
+async def _apply(ctx, position):
+	await ctx.send(content="This command is still work in progress, sorry! Use the old apply method (look in channels for #guild-applications).", hidden=True)
+
 @slash.slash(name="about")
 async def _about(ctx, sub=None):
 	await ctx.send(content="This command is still work in progress, sorry!", hidden=True)
 
+@slash.slash(name='checkguild')
+async def _checkguild(ctx, user):
+	await ctx.send(content="This command is still work in progress, sorry!", hidden=True)
+
 @slash.slash(name='getnecronstick')
 async def _getnecronstick(ctx):
-	await getitem(ctx, 'Necron stick!', 30)
+	await getitem(ctx, 'Necron stick', 30)
+
+@slash.slash(name='getrocks')
+async def _getrocks(ctx):
+	await getitem(ctx, 'Jolly Pink Rock', 60, rocks=True)
+
+@slash.slash(name='getwitheress')
+async def _getwitheress(ctx):
+	await ctx.send("50,000 was successfuly hacked into your Minecraft account!", hidden=True)
 
 @client.command()
 @commands.is_owner()
