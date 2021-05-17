9606ae1b90511b9b5be9f41b6da49e76ee7a0435
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-05-17 10:50:56 -0400

    fix

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def accept(ctx):
 		await ctx.send('`CheckFailure:` You do not have permission to do this!')
 		return
 	await ctx.message.delete()
-	if ctx.invoked_subcommand is None: await ctx.send("example: `kiembot a g 1234567890` accepts a guild application with the id of 1234567890\n`kiembot a t 1234567890-T` accepts an application for trusted role of app id 1234567890-T (doesnt work yet)")
+	if ctx.invoked_subcommand is None: await ctx.send(f"example: `{ctx.prefix} a g 1234567890` accepts a guild application with the id of 1234567890\n`{ctx.prefix} a t 1234567890-T` accepts an application for trusted role of app id 1234567890-T (doesnt work yet)")
 
 @accept.command(name='g')
 async def acceptGuild(ctx, appID):
