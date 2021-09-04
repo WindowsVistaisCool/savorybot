4d156f2cbdab38c34e37a353f6128d29cf9aae12
WindowsVistaisCool <kwrush024@pccsk12.com>
2021-09-04 09:37:41 -0400

    updated check to include my user

--- cogs/checks.py
+++ cogs/checks.py
@@ -1,11 +1,11 @@
 import discord
 
 async def owner_staff(ctx):
-    if ctx.author.id == 392502213341216769: return True
+    if ctx.author.id == 392502213341216769 or ctx.author.id == 406629388059410434: return True
     if ctx.guild.get_role(789593786287915010) in ctx.author.roles: return True
     return False
 
 async def owner_trusted(ctx):
-    if ctx.author.id == 392502213341216769: return True
+    if ctx.author.id == 392502213341216769 or ctx.author.id == 406629388059410434: return True
     if ctx.guild.get_role(789592055600250910) in ctx.author.roles: return True
     return False
